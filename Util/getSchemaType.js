29f95b7dd485b0153149ccf37b56e7509031de2d
Philipp Steckenbauer <42046147+philipp122@users.noreply.github.com>
2020-11-29 16:43:03 +0100

    Added JSDOC Comments for getSchemaType.js and changed exporting

--- Util/getSchemaType.js
+++ Util/getSchemaType.js
@@ -1,6 +1,14 @@
 const Type = require('@power-industries/typejs');
 
-module.exports = function getSchemaType(schema) {
+/**
+ * getSchemaType is a function checking if schema is an Object and has a property called type which has to be a String
+ * @param schema {Object} - The Schema to check
+ * @param schema.type {String} - The SchemaType to check
+ * @returns {String} - Returns schema.type
+ * @throws {TypeError} - Throws a TypeError if schema is not an Object
+ * @throws {TypeError} - Throws a TypeError if schema.type is not a String
+ */
+const getSchemaType = (schema) => {
 	if (!(schema instanceof Type.Object))
 		throw new TypeError('Expected schema to be an Object');
 
@@ -8,4 +16,6 @@ module.exports = function getSchemaType(schema) {
 		throw new TypeError('Expected schema.type to be a String');
 
 	return schema['type'].toLowerCase();
-}
+};
+
+module.exports = getSchemaType;
