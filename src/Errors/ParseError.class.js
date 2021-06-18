b326cc08736637afa49033f1d1bd7717e2164de5
Philipp Steckenbauer <42046147+philipp122@users.noreply.github.com>
2021-06-18 20:22:16 +0200

    Added ParseError.class.js and SchemaError.class.js as custom Error Classes

---
+++ src/Errors/ParseError.class.js
@@ -0,0 +1,6 @@
+/**
+ * ParseError - should be thrown if there was an Error during Parsing
+ */
+class ParseError extends Error {}
+
+module.exports = ParseError;
---
+++ src/Errors/SchemaError.class.js
@@ -0,0 +1,6 @@
+/**
+ * SchemaError - should be thrown if a given Schema is invalid
+ */
+class SchemaError extends Error {}
+
+module.exports = SchemaError;
